af03c3bd3d3909c4c4070908aca703665cc79696
obezeq <eortbra2812@g.educaand.es>
2024-09-26 21:37:48 +0200

    P1.2 - Ejercicios básicos

--- src/ej04.py
+++ src/ej04.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 try:
-    celsius = float(input("Introduce la temperatura en grados celsius: "))
-    fahrenheit = print(f"La temperatura {celsius} °C es {celsius*33.8} °F")
+    celsius = float(input("Introduce la temperatura en grados Celsius: "))
+    fahrenheit = celsius * 33.8
+
+    celsius = round(celsius, 2)
+    fahrenheit = round(fahrenheit, 2)
+
+    print(f"La temperatura en grados Farenheit es {celsius*33.8}ºF ({celsius}ºC)")
 except ValueError:
     print("No has introducido una temperatura!")
