81980d895a01a45d867dff43474cf8ace321fcbc
obezeq <eortbra2812@g.educaand.es>
2024-09-26 21:32:54 +0200

    P1.2 - Ejercicios básicos

--- src/ej03.py
+++ src/ej03.py
@@ -3,7 +3,10 @@
 ancho  = 17
 alto = 12.0
 
-print(ancho/2)
-print(ancho//2)
-print(alto/3)
-print(1 + (2 * 5))
+# print(ancho/2)
+# print(ancho//2)
+# print(alto/3)
+# print(1 + (2 * 5))
+
+division = ancho / alto
+print(f"{ancho} / {alto} = {division} y es de tipo {type(division)}")
