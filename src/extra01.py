5080fe56c3e6dbf58d86ee1d81a14b66cb4033cc
obezeq <obezequiel@iesrafaelalberti.es>
2024-10-04 13:21:46 +0200

    U1

--- src/extra01.py
+++ src/extra01.py
@@ -1,29 +1,60 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 
 
-def titular(frase):
-    frase = frase.split(' ')
-    parsed_frase = []
-    for w in frase:
-
-        if w != '':
+def titular_v1(frase):
+    frase = frase.strip()
     
-            first_letter = w[0].upper()
-            last_letter = w[-1]
-            mid_letter = w[1:-1]
+    nueva_frase = ""
+    nueva_palabra = True
+    
+    for c in frase:
+        if c == " ":
+            nueva_frase += c
+            nueva_palabra = True
+        elif nueva_palabra:
+            nueva_frase += c.upper()
+            nueva_palabra = False
+        else:
+            nueva_frase += c.lower()
             
-            letters = f"{first_letter}{mid_letter}{last_letter}"
-            parsed_frase.append(letters)
+    return nueva_frase
+
+def titular_v2(frase):
+    frase = frase.strip()
+    palabras = frase.split(' ')
+    nueva_frase = []
     
-            frase = ""
+    for palabra in palabras:
+        if palabra:
+            nueva_frase.append(palabra[0].upper() + palabra[1:].lower())
+            
+    return ' '.join(nueva_frase)
 
-    for pw in parsed_frase:
-        frase = frase + pw + " "
-    frase = frase[:-1]
-
-    print(frase)
+def titular_v3(frase):
+    frase = frase.strip()
+    palabras = frase.split(' ')
+    
+    c = 0
+    for p in range(len(palabras)):
+        if palabras[c] != '':
+            palabras[c] = palabras[c][0].upper() + palabras[c][1:].lower()
+            c += 1
+        else:
+            del palabras[c]
+            
+    return ' '.join(palabras)
     
 def main():
-    titular("   hola mundo desde python   ")
+    og_frase = input("[+] Introduce una frase: ")
+    v1 = titular_v1(og_frase)
+    v2 = titular_v2(og_frase)
+    v3 = titular_v3(og_frase)
+    
+    print(f"\nOriginal: {og_frase}")
+    print(f"titular_v1: {v1}")
+    print(f"titular_v2: {v2}")
+    print(f"titular_v3: {v3}")
+    
+    
 
 if __name__ == '__main__':
     main()
